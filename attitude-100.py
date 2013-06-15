4d4c1144844c83c068c9a76ac3bed1eeb5b3952a
X Deng <hiukuan.tang@gmail.com>
2013-06-15 17:12:06 +0900

    Added "Attitude - 100"
    In celebration of a hard lesson learned about how "attitude" changes
    your life XD

---
+++ attitude-100.py
@@ -0,0 +1,36 @@
+# Finds all English words, the value of whose individual characters, if 
+# mapped to a numbered list (1, 2, 3 ... 26), add up ot exactly 100.
+# Word list from SIL International Linguistics Department
+# at http://www-01.sil.org/linguistics/wordlists/english/
+# Inspired by the (in)famous HR gag of 'ATTITUTE' = 100%.
+# Github @hktang
+
+import time
+
+def find_perfect_words(word_list):
+	'''
+	Take a list of words and return in a list all "perfect" words, i.e., 
+	words adding up to exactly 100. 
+	'''	
+	chars = ['\r']
+	hits = []
+	start_time = time.time()
+	for c in "abcdefghijklmnopqrstuvwxyz":
+		chars.append(c)
+	with open(word_list) as dic:
+		for line in dic:
+			word = line.strip("\n\r")
+			score = 0
+			for c in word:
+				try:
+					score += chars.index(c)
+				except:
+					score += 0
+			if score == 100:
+				hits.append(word)
+	for hit in hits:
+		print hit
+	print "\nJob done.", str(len(hits)), "\"perfect\" words found in", \
+	      time.time() - start_time, "seconds. \n"
+
+find_perfect_words("vendor/words-en.txt")
